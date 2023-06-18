d9bd29b634c4ac1886dfcc3e95f23001dd8b78c8
isurusajith68 <isurusajith68@gmail.com>
2023-06-18 17:41:57 +0530

    tosat notifications

--- client/src/pages/QRCodeScannerPage.js
+++ client/src/pages/QRCodeScannerPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import axios from "axios";
 import QrReader from "react-qr-scanner";
+import toast, { Toaster } from "react-hot-toast";
 
 const QRScanner = () => {
   const [scannedData, setScannedData] = useState("");
@@ -25,21 +25,33 @@ const QRScanner = () => {
     console.error("QR code scan error:", error);
   };
 
-  const fetchTicketData = async (ticketCode) => {
-    try {
-      const response = await axios.get(
-        `http://localhost:5000/api/tickets/${ticketCode}`
-      );
-      const { ticket } = response.data;
-      setTicketData(ticket);
-    } catch (error) {
-      console.error("Error retrieving ticket:", error);
-      // Handle error state or display error message
-    }
-  };
+ const fetchTicketData = async (ticketCode) => {
+   try {
+     const response = await fetch(
+       `http://localhost:5000/api/tickets/${ticketCode}`,
+       {
+         headers: { "Content-Type": "application/json" },
+       }
+     );
+
+     if (response.status === 404) {
+       toast.error("Ticket not found.");
+     } else if (response.status === 200) {
+       const data = await response.json();
+       const ticket = data.ticket;
+       setTicketData(ticket);
+       toast.success("Successfully retrieved ticket.");
+     }
+   } catch (error) {
+     console.error("Error retrieving ticket:", error);
+    toast.success("Failed to retrieve ticket.");
+   }
+ };
+
 
   return (
     <div>
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
       <h2>QR Code Scanner</h2>
       <QrReader
         delay={300}
@@ -56,7 +68,7 @@ const QRScanner = () => {
       {ticketData.id && (
         <div>
           <h2>Ticket Data:</h2>
-          <p >ID: {ticketData.id}</p>
+          <p>ID: {ticketData.id}</p>
           <p>Ticket Code: {ticketData.ticket_code}</p>
           <p>passenger Name: {ticketData.passenger_name}</p>
           <p>Source: {ticketData.source}</p>
